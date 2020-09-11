51572697c41229c5c8e930a18b1674069528d7a4
Roberto Sampaio <ssampaio.roberto@gmail.com>
2020-09-11 10:24:25 -0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Roberto Sampaio on 02/09/20.
+//  Copyright © 2020 Roberto Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
